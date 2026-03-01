f56d8bf3d7804ad1d7049b4e7614703a237e5e33
Nick Engriss <nickengrissix@gmail.com>
2026-03-01 15:41:34 +0000

    Silence pyright by changing where TOKEN is defined

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import os
 import sys
 from dotenv import load_dotenv
 
-from bot import bot
 from core.state import (
     load_application_state,
     load_blacklist
 )
+
+from bot import bot
 
 # ⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻
 # Main Script
@@ -26,16 +27,16 @@ logging.basicConfig(
 log = logging.getLogger("Utility Bot")
 
 TOKEN = os.getenv("TOKEN")
-if not TOKEN:
-    raise RuntimeError("TOKEN environment variable not set.")
 
 async def main() -> None:
+    if not TOKEN:
+        raise RuntimeError("TOKEN environment variable not set.")
+
     log.info("Starting Discord connection")
-    try:
+    try:    
         await bot.start(TOKEN.strip())
     except Exception:
         log.exception("Bot crashed during runtime")
-        raise
 
 if __name__ == "__main__":
     try:
