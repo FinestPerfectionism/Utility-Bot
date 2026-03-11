c609908ec67b5ef1e733f572003a641c4ee8747c
Nick Engriss <nickengrissix@gmail.com>
2026-03-11 23:45:55 +0000

    .

--- commands/systems/leave/__init__.py
+++ commands/systems/leave/__init__.py
@@ -1,8 +1,13 @@
 from discord.ext import commands
 
+from ._base  import leave_group
 from .add    import LeaveAdd
 from .remove import LeaveRemove
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(LeaveAdd(bot))
     await bot.add_cog(LeaveRemove(bot))
+    bot.tree.add_command(leave_group)
+
+async def teardown(bot: commands.Bot) -> None:
+    bot.tree.remove_command(leave_group.name)
